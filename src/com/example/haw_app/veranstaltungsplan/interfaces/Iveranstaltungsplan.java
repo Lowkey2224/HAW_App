56c978c3664190fcb6ccda75519b2758abb5e912
ThePanther <bwagener@gmx.de>
2013-04-22 00:29:46 +0200

    JavaDoc für Iveranstaltungsplan

--- src/com/example/haw_app/veranstaltungsplan/interfaces/Iveranstaltungsplan.java
+++ src/com/example/haw_app/veranstaltungsplan/interfaces/Iveranstaltungsplan.java
@@ -4,12 +4,26 @@ import java.util.List;
 
 public interface Iveranstaltungsplan {
 	
+	/**
+	 * Erstellt, mit Hilfe der Liste der belegten Faecher, eine .ics Datei lokal. 
+	 */
 	public void exportieren();
 	
+	/**
+	 * Downloaded die aktuelle Termin .txt Datei vom Server und parst diese.
+	 */
 	public void aktualisieren();
 	
+	/**
+	 * Methode zum aendern der belegten Faecher.(Nur belegte Faecher werden auch exportiert.) 
+	 * @param faecher Liste der Namen der belgeten Faecher z.B. "BAI4-SE2".
+	 */
 	public void setzeBelegteFaecher(List<String> faecher);
 	
+	/**
+	 * Methode zum aendern der Datei die geparst werden soll.
+	 * @param file Datei die geparst wird.
+	 */
 	public void setzeDatei(String file);
 
 }
