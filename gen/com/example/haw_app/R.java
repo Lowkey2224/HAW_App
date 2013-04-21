4317724bda02f8ad1aa87af13cdd2169fac48588
Dominike Manietzki <domani2@web.de>
2013-04-21 18:20:38 +0200

    GUI - first Try

--- gen/com/example/haw_app/R.java
+++ gen/com/example/haw_app/R.java
@@ -10,6 +10,10 @@ package com.example.haw_app;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060000;
+        public static final int white=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,21 +25,50 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int essen=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int options=0x7f020002;
+        public static final int pool=0x7f020003;
+        public static final int schnitzel=0x7f020004;
+        public static final int social=0x7f020005;
+        public static final int stisys=0x7f020006;
+        public static final int veranstaltung=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090007;
+        public static final int button1=0x7f090000;
+        public static final int button2=0x7f090001;
+        public static final int button3=0x7f090002;
+        public static final int button4=0x7f090003;
+        public static final int button5=0x7f090004;
+        public static final int button6=0x7f090005;
+        public static final int button7=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int essensplan=0x7f030001;
+        public static final int optionen=0x7f030002;
+        public static final int pcpool=0x7f030003;
+        public static final int schnitzeljagd=0x7f030004;
+        public static final int socialfeatures=0x7f030005;
+        public static final int stisys=0x7f030006;
+        public static final int veranstaltungsplan=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int test=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int essensplan=0x7f050002;
+        public static final int meine_noten=0x7f050009;
+        public static final int optionen=0x7f050008;
+        public static final int pc_pool=0x7f050003;
+        public static final int schnitzeljagd=0x7f050007;
+        public static final int social_features=0x7f050005;
+        public static final int stisys=0x7f050006;
+        public static final int veranstaltungsplan=0x7f050004;
     }
     public static final class style {
         /** 
@@ -59,10 +92,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/haw_app/MainActivity.java
+++ src/com/example/haw_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.haw_app;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +19,38 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
 
-}
+	public void veranstaltungsClick(View view){
+	    	setContentView(R.layout.veranstaltungsplan);
+	}
+	    
+	public void essensClick(View view){
+	    	setContentView(R.layout.essensplan);
+	}
+	    
+	    public void socialClick(View view){
+	    	setContentView(R.layout.socialfeatures);
+	    }
+	    
+	    public void poolClick(View view){
+	    	setContentView(R.layout.pcpool);
+	    }
+	    
+	    public void stisysClick(View view){
+	    	setContentView(R.layout.stisys);
+	    }
+	    
+	    
+	    public void schnitzelClick(View view){
+	    	setContentView(R.layout.schnitzeljagd);
+	    }
+	    
+	    public void optionClick(View view){
+	    	setContentView(R.layout.optionen);
+	    }
+
+	}
+
+
+
