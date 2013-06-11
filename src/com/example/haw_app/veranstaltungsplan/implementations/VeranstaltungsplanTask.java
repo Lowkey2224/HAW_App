7f100d82e4c3be4528eaef0c65b955d7a8e229e3
ThePanther <bwagener@gmx.de>
2013-06-11 18:42:09 +0200

    VeranstaltungsplanTask hinzugefügt

---
+++ src/com/example/haw_app/veranstaltungsplan/implementations/VeranstaltungsplanTask.java
@@ -0,0 +1,23 @@
+package com.example.haw_app.veranstaltungsplan.implementations;
+
+import android.os.AsyncTask;
+
+public class VeranstaltungsplanTask extends AsyncTask<Veranstaltungsplan, Void, Veranstaltungsplan>{
+
+	private Veranstaltungsplan vp;
+	
+	@Override
+	protected Veranstaltungsplan doInBackground(Veranstaltungsplan... arg0) {
+		
+		this.vp = arg0[0];
+		
+		try {
+			vp.aktualisieren();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return vp;
+	}
+
+}
